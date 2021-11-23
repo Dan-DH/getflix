b959e0733bed7891d19673959c6da5b76a1f62bf
Brigita Sabutyte <b.sabutyte@gmail.com>
2021-11-23 17:03:37 +0100

    adding editing to contact.php

--- src/Frontend/contact.php
+++ src/Frontend/contact.php
@@ -4,22 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleB.css">
     <title>Contact us</title>
 </head>
 <body>
-    <h3>Contact Us</h3>
-    <form action="" method="post">
-        <label for="issue">Specify your issue:<br>
+<div class="navbar">
+        <h1>Getflix</h1>
+        <div class="buttons">
+            <button type="button" id="home" href="main.php">HOME</button>
+            <button type="button" id="logout" href="login.php">LOG OUT</button>
+        </div>
+    </div>
+
+    <main>
+<div class="fill_form">
+    <form action="contact.php" method="post">
+    <h2>Contact Us</h2><br>
+    <input type="email" name="email" placeholder="Please,enter your email" id="email">
+        <label for="issue_type">
         <select name="issue" id="issue_type">
-            <option value="not_loading">Movies not loading</option>
-            <option value="cannot_update">Cannot update my profile info</option>
-            <option value="other">Other</option>
-        </select><br>
-        Tell us more about your issue:<br>
-        <textarea name="issue" id="problem" cols="30" rows="10"></textarea>
+            <option name="issue" value="select">Please, specify your issue</option>
+            <option value="not_loading" name="issue">Movies not loading</option>
+            <option value="cannot_update" name='issue'>Cannot update my profile info</option>
+            <option value="other" name="issue">Other</option>
+        </select><br><br>
+        
+        <textarea name="message" id="problem" cols="30" rows="10" placeholder="Tell us more about your issue" ></textarea>
         </label>
         <br>
-        <button>Send</button>
+        <button id='submit'>Send</button>
     </form>
+    </div>
+    </main>
 </body>
 </html>
