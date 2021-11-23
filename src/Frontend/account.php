74678b8a1364bdb324d077892561e51349f8e485
Dan-DH <daniel.diaz.hdez@gmail.com>
2021-11-23 14:19:16 +0100

    added account page

---
+++ src/Frontend/account.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="account.css">
+    <title>Account</title>
+</head>
+<body>
+<div class="navbar">
+        <h1>Getflix</h1>
+        <div class="buttons">
+            <a href="./main.php"><button type="button" id="home">Home</button></a>
+            <a href="./login.php"><button type="button" id="logout">Log out</button></a>
+        </div>
+    </div>
+</body>
+</html>
