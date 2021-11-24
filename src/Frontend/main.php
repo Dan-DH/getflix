b0742a61aee0871e80746f12477f8f189f12d76a
Shivani Khatri <khatri.shivani96@gmail.com>
2021-11-24 16:08:50 +0100

    carousel main page

--- src/Frontend/main.php
+++ src/Frontend/main.php
@@ -111,12 +111,42 @@
     
 <main>
 <div class="carousel" data-flickity='{ "groupCells": true}'>
-    <div class="carousel-cell"></div>
-    <div class="carousel-cell"></div>
-    <div class="carousel-cell"></div>
-    <div class="carousel-cell"></div>
-    <div class="carousel-cell"></div>
-    <div class="carousel-cell"></div>
+    <div class="carousel-cell">
+    <img data-tab="bright" src="http://www.thebrandage.com/assets/image/uploads/haberler/Bright_TUR.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="avatar" src="https://i.pinimg.com/736x/a4/23/f8/a423f86593029b7d2a6d9f1e1fd1e406---movies-movies-to-watch-online.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="thor" src="https://mikeantjones.files.wordpress.com/2012/04/thor-film-poster.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="john" src="https://images-na.ssl-images-amazon.com/images/I/91H06HPhX%2BL._SY717_.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="deadpool2" src="https://icdn3.digitaltrends.com/image/deadpool-2-thanksgiving-poster-1294x2048.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="fight" src="https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="bright" src="http://www.thebrandage.com/assets/image/uploads/haberler/Bright_TUR.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="avatar" src="https://i.pinimg.com/736x/a4/23/f8/a423f86593029b7d2a6d9f1e1fd1e406---movies-movies-to-watch-online.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="thor" src="https://mikeantjones.files.wordpress.com/2012/04/thor-film-poster.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="john" src="https://images-na.ssl-images-amazon.com/images/I/91H06HPhX%2BL._SY717_.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="deadpool2" src="https://icdn3.digitaltrends.com/image/deadpool-2-thanksgiving-poster-1294x2048.jpg"/>
+    </div>
+    <div class="carousel-cell">
+    <img data-tab="fight" src="https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg"/>
+    </div>
     </div>
 
     </main>
